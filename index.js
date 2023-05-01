cca2c0b7d2f346889e8618d6b0bbb73350ed0e48
Elliot Hatch <elliot.hatch@gmail.com>
2023-05-01 05:41:45 -0600

    Start using dynamic resources and json-ld processors

--- index.js
+++ index.js
@@ -16,35 +16,15 @@ module.exports = {
 				sitePaths: ['site'],
 				processors: {
 					pre: [{
-						name: 'self'
+						name: 'ld:self'
 					}],
-					post: [{
-						// name: 'matchProfile',
-						// options: {
-						// 	profile: '/schema/post',
-						// 	processors: [{
-						// 		name: 'markdown',
-						// 		options: {
-						// 			fromUri: './post.md',
-						// 			to: 'bodyHtml',
-						// 		}
-						// 	}]
-						// }
-						// }
-						name: 'index',
-						options: {
-							property: '_links.profile.href',
-							embed: ['title', 'date-created']
-						}
-					}, {
-							name: 'resourceGraph'
-					}]
 				},
-				stateProcessors: {
-					pre: [{
-						name: 'self'
-					}]
-				}
+				dynamicResources: [{
+					name: 'index',
+					options: {
+						property: '@type'
+					}
+				}]
 			},
 			renderer: {
 				templatePaths: ['templates'],
@@ -59,51 +39,7 @@ module.exports = {
 					'https://schema.org/AboutPage': 'layouts/about.hbs',
 					'https://schema.org/Test': 'layouts/test.hbs',
 				},
-				context: {
-					title: 'elliot.zone',
-					navLinks: {
-						// "store": {
-						// 	"href": "/store",
-						// 	"title": "Store"
-						// },
-						// "projects": {
-						// 	"href": "/projects",
-						// 	"title": "Projects"
-						// },
-						"about": {
-							"href": "/about",
-							"title": "About"
-						},
-						"test1": {
-							"href": "/test1",
-							"title": "test1"
-						},
-						"test2": {
-							"href": "/test2",
-							"title": "test2"
-						},
-						"test3": {
-							"href": "/test3",
-							"title": "test3"
-						},
-					},
-					footerLinks: [{
-						"title": "github",
-						"href": "https://github.com/elliothatch/"
-					}, {
-						"title": "npm",
-						"href": "https://www.npmjs.com/~elliothatch"
-					}, {
-						"title": "twitter",
-						"href": "https://twitter.com/neurotischism"
-					}, {
-						"title": "fedi",
-						"href": "https://weirder.earth/@neurotischism"
-					}, {
-						"title": "patreon",
-						"href": "https://www.patreon.com/neurotischism"
-					}]
-				}
+				context: '/site.json',
 			},
 			build: {
 				buildSteps: {
